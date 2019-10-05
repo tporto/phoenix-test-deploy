3e3f2e3cb8cace297e8529eed6527362b07bbf0c
Thiago Porto <tporto88@gmail.com>
2019-10-05 00:37:26 -0300

    ...

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,9 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :phoenix_hello, PhoenixHelloWeb.Endpoint,
-  url: [host: "144.202.38.64", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [host: "localhost", port: 80],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  code_reloader: false
 
 # Do not print debug messages in production
 config :logger, level: :info
