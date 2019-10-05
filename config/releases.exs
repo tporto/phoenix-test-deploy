5331a2c6bdfead32749bdaa64372163fb716e70e
Thiago Porto <tporto88@gmail.com>
2019-10-04 23:30:11 -0300

    first commit

--- config/releases.exs
+++ config/releases.exs
@@ -4,12 +4,12 @@
 # remember to add this file to your .gitignore.
 import Config
 
-secret_key_base =
-  System.get_env("SECRET_KEY_BASE") ||
-    raise """
-    environment variable SECRET_KEY_BASE is missing.
-    You can generate one by calling: mix phx.gen.secret
-    """
+secret_key_base = "${SECRET_KEY_BASE}"
+  #System.get_env("SECRET_KEY_BASE") ||
+  #  raise """
+  #  environment variable SECRET_KEY_BASE is missing.
+  #  You can generate one by calling: mix phx.gen.secret
+  #  """
 
 config :phoenix_hello, PhoenixHelloWeb.Endpoint,
   http: [:inet6, port: String.to_integer(System.get_env("PORT") || "80")],
