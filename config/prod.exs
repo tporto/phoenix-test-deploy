6a6cc4ab68bb3f8c1d9b85a23e2baf8b9440c153
Thiago Porto <tporto88@gmail.com>
2019-10-05 00:34:56 -0300

    ...

--- config/prod.exs
+++ config/prod.exs
@@ -11,8 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :phoenix_hello, PhoenixHelloWeb.Endpoint,
   url: [host: "144.202.38.64", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json",
-  code_reloader: false
+  cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
